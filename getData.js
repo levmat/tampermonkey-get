f8f11e51f3121418194e56d48aa510ee8fb9c5a0
Matjes <realmatjes@pm.me>
2022-09-02 21:06:37 +0200

    implement getData function

---
+++ getData.js
@@ -0,0 +1,22 @@
+function getData(endpoint, callback = () => null) {
+	return GM.xmlHttpRequest({
+		method: "GET",
+		headers: { accept: "application/json" },
+		url: endpoint,
+		onload: ({ responseText: data }) => {
+			data = JSON.parse(data);
+			data.status === 200 && callback(data);
+		},
+		onabort: (ex) => console.error("[ABORT] Oh no, something went wrong", ex),
+		onerror: (ex) => console.error("[ERROR] Oh no, something went wrong", ex),
+	});
+}
+
+// Example
+// Obtain data from teh endpoint, then run the callback function
+getData("https://icanhazdadjoke.com", showJoke);
+
+function showJoke(data) {
+	const { joke } = data;
+	console.log(joke);
+}
